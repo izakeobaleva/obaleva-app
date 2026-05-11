174b7b15593fad72533248831d55fb346ba8bb65
izakeobaleva <obalevaouro@gmail.com>
2026-05-11 18:02:14 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>OBALEVA - Seu transporte</h1>
+      <p>Aplicativo em construção. Em breve você poderá solicitar corridas!</p>
+    </div>
+  );
+}
+
+export default App;
