e74d90e8680831ab529260b965fff6b8c3cd05df
[dyad] <obalevaouro@gmail.com>
2026-05-12 01:42:45 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { AuthProvider } from './contexts/AuthContext';
-import { Login } from './pages/Login';
-import { RegisterPassenger } from './pages/RegisterPassenger';
-import { RegisterDriver } from './pages/RegisterDriver';
+import Login from './pages/Login';
+import RegisterPassenger from './pages/RegisterPassenger';
+import RegisterDriver from './pages/RegisterDriver';
 
 function App() {
   return (
