6713c30463e7d545e5c4f624ba7ccc3bd8ccd4ed
[dyad] <obalevaouro@gmail.com>
2026-05-12 01:41:34 -0300

    Updated src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import { AuthProvider } from './contexts/AuthContext';
+import { Login } from './pages/Login';
+import { RegisterPassenger } from './pages/RegisterPassenger';
+import { RegisterDriver } from './pages/RegisterDriver';
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterPassenger />} />
+          <Route path="/register-driver" element={<RegisterDriver />} />
+        </Routes>
+        <Toaster position="top-center" richColors />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
