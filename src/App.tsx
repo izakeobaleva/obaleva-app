0ecde83ad8b33cf580dff1bcd7ca022b6440b43a
[dyad] <obalevaouro@gmail.com>
2026-05-12 17:36:43 -0300

    [dyad] wrote 1 file(s), renamed 12 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,40 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { AuthProvider } from './contexts/AuthContext';
-import Login from './pages/Login';
-import RegisterPassenger from './pages/RegisterPassenger';
-import RegisterDriver from './pages/RegisterDriver';
-import ForgotPassword from './pages/ForgotPassword';
-import UpdatePassword from './pages/UpdatePassword';
-import PassengerDashboard from './pages/PassengerDashboard';
-import DriverDashboard from './pages/DriverDashboard';
+import Entrar from './pages/Entrar';
+import CadastroPassageiro from './pages/CadastroPassageiro';
+import CadastroMotorista from './pages/CadastroMotorista';
+import EsqueciSenha from './pages/EsqueciSenha';
+import AtualizarSenha from './pages/AtualizarSenha';
+import PainelPassageiro from './pages/PainelPassageiro';
+import PainelMotorista from './pages/PainelMotorista';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import Trips from './pages/Trips';
-import TripDetails from './pages/TripDetails';
-import Earnings from './pages/Earnings';
-import AdminDashboard from './pages/AdminDashboard';
-import NotFound from './pages/NotFound';
+import Viagens from './pages/Viagens';
+import DetalhesViagem from './pages/DetalhesViagem';
+import Ganhos from './pages/Ganhos';
+import PainelAdmin from './pages/PainelAdmin';
+import NaoEncontrada from './pages/NaoEncontrada';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<RegisterPassenger />} />
-          <Route path="/register-driver" element={<RegisterDriver />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-          <Route path="/passenger" element={<PassengerDashboard />} />
-          <Route path="/driver" element={<DriverDashboard />} />
+          <Route path="/" element={<Entrar />} />
+          <Route path="/register" element={<CadastroPassageiro />} />
+          <Route path="/register-driver" element={<CadastroMotorista />} />
+          <Route path="/forgot-password" element={<EsqueciSenha />} />
+          <Route path="/update-password" element={<AtualizarSenha />} />
+          <Route path="/passenger" element={<PainelPassageiro />} />
+          <Route path="/driver" element={<PainelMotorista />} />
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/trips" element={<Trips />} />
-          <Route path="/trips/:id" element={<TripDetails />} />
-          <Route path="/earnings" element={<Earnings />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/trips" element={<Viagens />} />
+          <Route path="/trips/:id" element={<DetalhesViagem />} />
+          <Route path="/earnings" element={<Ganhos />} />
+          <Route path="/admin" element={<PainelAdmin />} />
+          <Route path="*" element={<NaoEncontrada />} />
         </Routes>
         <Toaster position="top-center" richColors />
       </BrowserRouter>
