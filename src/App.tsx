49ace6dd4705311276026b36c9de232074cf95b8
[dyad] <obalevaouro@gmail.com>
2026-05-12 01:23:20 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
-import { Login } from './pages/Login';
-import { RegisterPassenger } from './pages/RegisterPassenger';
-import { RegisterDriver } from './pages/RegisterDriver';
-import { PassengerDashboard } from './pages/PassengerDashboard';
-import { DriverDashboard } from './pages/DriverDashboard';
-import { AdminDashboard } from './pages/AdminDashboard';
+import Login from './pages/Login';
+import RegisterPassenger from './pages/RegisterPassenger';
+import RegisterDriver from './pages/RegisterDriver';
+import PassengerDashboard from './pages/PassengerDashboard';
+import DriverDashboard from './pages/DriverDashboard';
+import AdminDashboard from './pages/AdminDashboard';
 
 function AppRoutes() {
   const { user, profile, loading } = useAuth();
