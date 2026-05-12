6b68240982cb0d661fa11ac516125a4260da8446
[dyad] <obalevaouro@gmail.com>
2026-05-12 03:03:09 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { AuthProvider } from './contexts/AuthContext';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import PassengerDashboard from './pages/PassengerDashboard';
+import DriverDashboard from './pages/DriverDashboard';
+import Trips from './pages/Trips';
+import TripDetails from './pages/TripDetails';
+import Profile from './pages/Profile';
+import Earnings from './pages/Earnings';
+import RegisterPassenger from './pages/RegisterPassenger';
+import RegisterDriver from './pages/RegisterDriver';
+import ForgotPassword from './pages/ForgotPassword';
+import UpdatePassword from './pages/UpdatePassword';
+import AdminDashboard from './pages/AdminDashboard';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -10,6 +23,19 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/passenger" element={<PassengerDashboard />} />
+          <Route path="/driver" element={<DriverDashboard />} />
+          <Route path="/trips" element={<Trips />} />
+          <Route path="/trips/:id" element={<TripDetails />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/earnings" element={<Earnings />} />
+          <Route path="/register" element={<RegisterPassenger />} />
+          <Route path="/register-driver" element={<RegisterDriver />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/update-password" element={<UpdatePassword />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Toaster position="top-center" richColors />
       </BrowserRouter>
