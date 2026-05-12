218c2ceca92c76455e9541aafdab05c9bce22d18
[dyad] <obalevaouro@gmail.com>
2026-05-12 00:02:57 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -39,48 +39,6 @@ function App() {
           <Route path="/update-password" element={<UpdatePassword />} />
           <Route path="/register/passenger" element={<RegisterPassenger />} />
           <Route path="/register/driver" element={<RegisterDriver />} />
-          <Route path="<dyad-write path="src/App.tsx" description="Add TripDetails route to the router configuration">
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { useAuth } from './contexts/AuthContext'
-import { Toaster } from 'sonner'
-import Landing from './pages/Landing'
-import Login from './pages/Login'
-import ForgotPassword from './pages/ForgotPassword'
-import UpdatePassword from './pages/UpdatePassword'
-import RegisterPassenger from './pages/RegisterPassenger'
-import RegisterDriver from './pages/RegisterDriver'
-import Home from './pages/Home'
-import PassengerDashboard from './pages/PassengerDashboard'
-import DriverDashboard from './pages/DriverDashboard'
-import AdminDashboard from './pages/AdminDashboard'
-import Trips from './pages/Trips'
-import TripDetails from './pages/TripDetails'
-import Earnings from './pages/Earnings'
-import Profile from './pages/Profile'
-import NotFound from './pages/NotFound'
-
-function App() {
-  const { user, loading } = useAuth()
-
-  if (loading) {
-    return (
-      <div className="min-h-screen bg-[#0F0B1A] flex items-center justify-center">
-        <div className="animate-spin h-8 w-8 border-2 border-[#F4D03F] border-t-transparent rounded-full" />
-      </div>
-    )
-  }
-
-  if (!user) {
-    return (
-      <>
-        <Toaster position="top-center" richColors />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-          <Route path="/register/passenger" element={<RegisterPassenger />} />
-          <Route path="/register/driver" element={<RegisterDriver />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </>
