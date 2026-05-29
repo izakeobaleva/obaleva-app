5723003e5c713e8a89d031671b13ba794f1b642e
[dyad] <obalevaouro@gmail.com>
2026-05-29 02:01:38 -0300

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PermissionLocation from './pages/PermissionLocation';
 import PermissionNotification from './pages/PermissionNotification';
 import Login from './pages/Login';
@@ -8,10 +8,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/permission-location" replace />} />
         <Route path="/permission-location" element={<PermissionLocation />} />
         <Route path="/permission-notification" element={<PermissionNotification />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<MainScreen />} />
+        <Route path="/home" element={<MainScreen />} />
       </Routes>
     </BrowserRouter>
   );
